3653193929642b3634af4739e3eec0d5c97b616f
Gianni Settino <g@miltonandparc.com>
2018-02-23 22:15:24 -0500

    Fuck Storyboards

---
+++ Trade Diary/Controllers/TradeTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TradeTableViewController.swift
+//  Trade Diary
+//
+//  Created by Gianni Settino on 2018-02-23.
+//  Copyright © 2018 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class TradeTableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
